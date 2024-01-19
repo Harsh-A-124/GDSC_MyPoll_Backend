7ccec24bf45da8917bb2ab569102e909364e491f
Harsh-A-124 <153938396+Harsh-A-124@users.noreply.github.com>
2024-01-19 19:24:11 +0530

    Delete dbconfig directory

--- dbconfig/dbconfig.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-MONGO_URI = "mongodb+srv://hankstark1204:lmfao1204@gdscmypolldb.kmjfnvw.mongodb.net/"
-
-conn = MongoClient(MONGO_URI)
-db = conn.gdsc_mypoll_backend_db
