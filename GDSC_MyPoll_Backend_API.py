09c1105b2c5f6a20bce3fc01f91e4e98bc81c1a8
Harsh-A-124 <153938396+Harsh-A-124@users.noreply.github.com>
2024-01-19 19:23:46 +0530

    Delete GDSC_MyPoll_Backend_API.py

--- GDSC_MyPoll_Backend_API.py
+++
@@ -1,8 +0,0 @@
-from typing import Union
-from fastapi import FastAPI
-from pymongo import MongoClient
-from dbconfig.dbconfig import conn
-from routes.routes import route
-
-app = FastAPI()
-app.include_router(route)
