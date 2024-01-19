b23c0e0b91930a520d411128586bd9d14af4bbf5
Harsh-A-124 <153938396+Harsh-A-124@users.noreply.github.com>
2024-01-19 15:56:30 +0530

    Add files via upload

---
+++ models/models.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, Field
+
+class User(BaseModel):
+    firebase_uid : str = Field(default="uninitialized")
+    created_at : str = Field(default="uninitialized")
+    name : str
+    email : str
+    mobileno : str
+    password : str
+    complaints : list[str] = Field(default=None)
+    donations : list[str] = Field(default=None)
+    location : str
+    type : str = Field(default="citizen")
+
+class Complaints(BaseModel):
+    cid : str = Field(default="uninitialized")
+    user : User
+    timestamp : str
+    category : str
+    description : str
+    area : str
+    address : str
+    # images : str | list[str]
+    images : str
+
+class Donation(BaseModel):
+    did : str = Field(default="uninitialized")
+    user : list[User]
+    timestamp : str
+    amount : str
+
+class Notifications(BaseModel):
+    nid : str = Field(default="uninitialized")
+    timestamp : str
+    user : User
+    area : str
+    message : str
